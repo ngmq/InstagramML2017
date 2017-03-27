84bcf73aff491643b8493ffd679eba75315568b0
ngmq <manhquan233@gmail.com>
2017-03-27 14:58:27 +0200

    Add file Y tuong

--- ReadData.py
+++ ReadData.py
@@ -37,14 +37,18 @@ f.close()
 # print data[3].get("id")
 # print data[4].get("id")
 # print "============================="
-print type(data[0].get("posts")) # type = list
-p0 = data[0].get("posts")
-print len(p0)
-print type(p0[0])
-print p0[0].keys()
-print "============================="
-print p0[0].get('updated')
-print "============================="
-print p0[0].get('instagram').keys()
-print "============================="
-print p0[0].get('annotations').keys()
+# print type(data[0].get("posts")) # type = list
+# p0 = data[0].get("posts")
+# print len(p0)
+# print type(p0[0])
+# print p0[0].keys()
+# print "============================="
+# print p0[0].get('updated')
+# print "============================="
+# print p0[0].get('instagram').keys()
+# print "============================="
+# print p0[0].get('annotations').keys()
+# print "============================="
+p1 = data[1].get('posts')
+print p1[0].get('instagram').get('code')
+print p1[0].get('instagram').get('likes').get('count')
